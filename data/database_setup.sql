83d163b2b7154789610c224933dd2dc2422af360
NTIVUNWA250 <g.ntivunwa@alustudent.com>
2026-01-23 01:22:17 +0200

    Basic DDL in MySQL for tables user, role, transactions,..

---
+++ data/database_setup.sql
@@ -0,0 +1,50 @@
+#This is a mysql code to make MOMO transanction database using MySQL
+
+CREATE DATABASE momodb;
+
+CREATE TABLE User (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR,
+	phone VARCHAR(50),
+	password VARCHAR(50)
+);
+
+CREATE TABLE Role (
+	role_id INT AUTO_INCREMENT PRIMARY KEY,
+	role_name VARCHAR(50)
+);
+
+CREATE TABLE UserRole (
+	user_id INT NOT NULL,
+	role_id INT NOT NULL,
+	PRIMARY KEY (user_id, role_id),
+	FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (role_id) REFERENCES Role(role_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Categories (
+	category_id INT AUTO_INCREMENT PRIMARY KEY,
+	category_name VARCHAR(50)
+);
+
+CREATE TABLE Transactions (
+	transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+	amount FLOAT NOT NULL,
+	timestamp DATETIME NOT NULL,
+	currency VARCHAR(50),
+	reciever_id INT NOT NULL,
+	sender_id INT NOT NULL,
+	category_id INT NOT NULL,
+	FOREIGN KEY (reciever_id) REFERENCES User(user_id),
+	FOREIGN KEY (sender_id) REFERENCES User(user_id),
+	FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+
+CREATE TABLE System_logs (
+	log_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	endpoint VARCHAR(50),
+	status_code INT NOT NULL,
+	execution_time_ms FLOAT,
+	FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
